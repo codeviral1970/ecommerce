9307f16b46c70fd171cbdf4909c3d6333a78c5d3
Man <manbanhduc@gmail.com>
2021-12-14 15:34:23 +0100

    Add field in entity

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,30 +5,60 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'constraints' => [new Length(['min' => 2, 'max' =>50])],
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ])
+            ->add('lastname',  TextType::class, [
+                'constraints' => [new Length(['min' => 2, 'max' =>50])],
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
             ->add('email', EmailType::class, [
+                'constraints' => [new Length(['min' => 2, 'max' =>50])],
                 'label' => 'Votre email',
                 'attr' => [
-                    'placeholder' => 'Saisir votre email'
+                    'placeholder' => 'Merci de saisir votre email'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password',RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe n\'est pas identique',
                 'label' => 'Votre mot de passe',
-                'attr' => [
-                    'placeholder' => 'Saisir votre mot de passe'
+                'required' =>true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => "Saisir votre mot de passe"
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => "Re saisir votre mot de passe"
+                    ]
                 ]
             ])
-            ->add('Save', SubmitType::class, [
-                'label' => 'S\'enregistrer'
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregister'
             ])
         ;
     }
